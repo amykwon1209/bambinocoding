b73d251502ba5205761a1ba886995eefd2b20b9b
amykwon1209 <40872001+amykwon1209@users.noreply.github.com>
2018-07-29 00:12:50 -0700

    Add files via upload

---
+++ bambino.js
@@ -0,0 +1,47 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn_menu')) {
+
+    var dropdowns2 = document.getElementsByClassName("dropdown_menu-content");
+    var i;
+    for (i = 0; i < dropdowns2.length; i++) {
+      var openDropdown = dropdowns2[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
